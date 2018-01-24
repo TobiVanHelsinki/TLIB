fd9328eeb52baa833cb15fecaac8542031bd81b9
Tobias Pabst <TobivanHelsinki@live.de>
2018-01-24 20:18:10 +0100

    changed tlib to shared project

---
+++ TLIB/CrossPlatform/CrossPlatformHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TLIB.CrossPlatformHelper
+{
+    internal static class Threading
+    {
+        public static async Task SleepMilliSeconds(int ms)
+        {
+#if UWP
+            await Task.Delay(TimeSpan.FromMilliseconds(ms));
+#else
+            Thread.Sleep(ms);
+#endif
+
+        }
+    }
+    internal static class PrintOut
+    {
+        public static void WriteLine( string s)
+        {
+#if UWP
+            System.Diagnostics.Debug.WriteLine(s);
+#else
+            Console.Write(s);
+#endif
+
+        }
+    }
+}
---
+++ TLIB/Server/HttpServer.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using TLIB.Server;
+
+namespace TLIB.Net
+{
+    internal abstract class HttpServer
+    {
+
+        protected int port;
+        TcpListener listener;
+        public bool is_active = true;
+        IPAddress ipaddr;
+
+        public HttpServer(int port, IPAddress ipaddr)
+        {
+            this.port = port;
+            this.ipaddr = ipaddr;
+        }
+
+        public async void listen()
+        {
+            listener = new TcpListener(ipaddr, port);
+            listener.Start();
+            while (is_active)
+            {
+                TcpClient socket = await listener.AcceptTcpClientAsync();
+                HttpProcessor processor = new HttpProcessor(socket, this);
+#if UWP
+                IAsyncAction asyncAction = Windows.System.Threading.ThreadPool.RunAsync(
+                (workItem) =>
+                {
+                    processor.process();
+                });
+                await Task.Delay(TimeSpan.FromMilliseconds(1));
+#else
+                Thread thread = new Thread(new ThreadStart(processor.process));
+                thread.Start();
+                Thread.Sleep(1);
+#endif
+
+            }
+        }
+
+        public abstract void handleGETRequest(HttpProcessor p);
+        public abstract void handlePOSTRequest(HttpProcessor p, StreamReader inputData);
+    }
+
+
+}
+
