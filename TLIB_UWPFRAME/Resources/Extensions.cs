bc34e4bdcfb7839a834212ab463e2460ac952b0b
Tobias Pabst <TobivanHelsinki@live.de>
2018-02-07 14:10:41 +0100

    added addrange for observl lists

---
+++ TLIB_UWPFRAME/Resources/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TLIB_UWPFRAME.Resources
+{
+    public static class CollectionExtension
+    {
+        public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> param)
+        {
+            foreach (var item in param)
+            {
+                source.Add(item);
+            }
+        }
+    }
+}
