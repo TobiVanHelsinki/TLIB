ca05bc173891354f4516bc9a07b3f0f579fdfbd1
Tobias Pabst <TobivanHelsinki@live.de>
2018-02-10 20:37:06 +0100

    io adjustments and more helpers

---
+++ TLIB_UWPFRAME/Resources/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TLIB_UWPFRAME.Resources
+{
+    public class Helper
+    {
+        public static IEnumerable<PropertyInfo> GetProperties(object obj, Type type)
+        {
+            //var t = obj.GetType().GetProperties();
+            return obj.GetType().GetProperties().Where(p => p.CustomAttributes.Any(c => c.AttributeType == type));
+        }
+
+
+    }
+}
