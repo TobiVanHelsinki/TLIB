a70414236e32a1a7cfac86726023cb4952604972
Tobias Pabst <TobivanHelsinki@live.de>
2018-03-05 13:25:50 +0100

    improovements on io

---
+++ TLIB_UWPFRAME/IO/Definitions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TLIB_UWPFRAME.IO
+{
+
+    [Serializable]
+    public class IsOKException : Exception
+    {
+        public IsOKException()
+        {
+        }
+
+        public IsOKException(string message) : base(message)
+        {
+        }
+
+        public IsOKException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
