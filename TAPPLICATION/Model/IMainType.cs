3890d288041839491e03782be26d3178a3a3988f
Tobi van Helsinki <TobivanHelsinki@live.de>
2018-04-16 20:19:31 +0200

    reordering

--- TAPPLICATION/Model/IMainType.cs
+++ TAPPLICATION/Model/IMainType.cs
@@ -1,23 +1,20 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using TAMARIN.IO;
 
 namespace TAPPLICATION.Model
 {
     public interface IMainType
     {
-        string MakeName();
-        // Admin Version Numbers
         string APP_VERSION_NUMBER { get; } 
         string FILE_VERSION_NUMBER { get; }
 
         FileInfoClass FileInfo { get; set; }
 
         event EventHandler SaveRequest;
-
-        #region AUTO_SAVE_STUFF 
-        [Newtonsoft.Json.JsonIgnore]
+        [JsonIgnore]
         bool HasChanges { get; set; }
-        #endregion
 
+        string MakeName();
     }
 }
