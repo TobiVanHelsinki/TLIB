8394f6ab0fd87f8285abc1b9e7265366ef42c8f6
tobiv <tobivanhelsinki@live.de>
2018-06-27 17:14:14 +0200

    added math and random functions

--- TLIB/Helper/CollectionHelper.cs
+++ TLIB/Helper/CollectionHelper.cs
@@ -7,6 +7,59 @@ namespace TLIB
 {
     public static class CollectionHelper
     {
+        public static bool TrueForAll<TSource>(this IEnumerable<TSource> source, Predicate<TSource> match)
+        {
+            if (match == null)
+            {
+                throw new System.ArgumentNullException();
+            }
+            return source.Aggregate(true, (b, i) => b && match.Invoke(i));
+        }
+        public static IEnumerable<IEnumerable<TSource>> CartesianProduct<TSource>(this IEnumerable<IEnumerable<TSource>> source)
+        {
+            IEnumerable<IEnumerable<TSource>> emptyProduct = new[] { Enumerable.Empty<TSource>() };
+            return source.Aggregate(
+              emptyProduct,
+              (accumulator, sequence) =>
+                from accseq in accumulator
+                from item in sequence
+                select accseq.Concat(new[] { item }));
+        }
+        /// <summary>
+        /// works for source.Count() values 0-30, 190
+        /// works not for source.Count() values 31-35, 191-200
+        /// other not tested
+        /// </summary>
+        /// <typeparam name="TSource"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<TSource>> ToPowerSet<TSource>(this IEnumerable<TSource> source)
+        {
+            //https://codereview.stackexchange.com/questions/51938/get-distinct-combinations-of-numbers
+            var ret = from m in Enumerable.Range(0, 1 << source.Count())
+                      select
+                          from i in Enumerable.Range(0, source.Count())
+                          where (m & (1 << i)) != 0
+                          select source.ElementAt(i);
+            int sourceCount = source.Count();
+            double shouldCount = System.Math.Pow(2, sourceCount);
+            long retCount = ret.Count();
+            return shouldCount == retCount ? ret : throw new Exception("Wrong count after calculation");
+        }
+
+        public static TSource RandomElement<TSource>(this IEnumerable<TSource> source)
+        {
+            return source.ElementAtOrDefault(StaticRandom.Next(0, source.Count()));
+        }
+        public static IEnumerable<TSource> RandomElements<TSource>(this IEnumerable<TSource> source, int number)
+        {
+            var x = new List<TSource>();
+            for (int i = 0; i < number; i++)
+            {
+                x.Add(RandomElement(source));
+            }
+            return x;
+        }
         public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> param)
         {
             foreach (var item in param)
---
+++ TLIB/Helper/StaticRandom.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TLIB
+{
+    public static class StaticRandom
+    {
+        public static Random r = new Random(DateTime.Now.Second);
+
+        //
+        // Zusammenfassung:
+        //     Gibt eine nicht negative Zufallsganzzahl zurück.
+        //
+        // Rückgabewerte:
+        //     Eine 32-Bit-Ganzzahl mit Vorzeichen, die größer als oder gleich 0 und kleiner
+        //     ist als System.Int32.MaxValue.
+        public static int Next()
+        {
+            return r.Next();
+        }
+        //
+        // Zusammenfassung:
+        //     Gibt eine Zufallsganzzahl zurück, die in einem angegebenen Bereich liegt.
+        //
+        // Parameter:
+        //   minValue:
+        //     Die inklusive untere Grenze der zufälligen Zahl zurückgegeben.
+        //
+        //   maxValue:
+        //     Die exklusive obere Grenze der zufälligen Zahl zurückgegeben. maxValuemuss größer
+        //     als oder gleich minValue.
+        //
+        // Rückgabewerte:
+        //     Eine 32-Bit-Ganzzahl mit Vorzeichen größer oder gleich minValue und weniger als
+        //     maxValue; ist, enthält des Bereichs der Rückgabewerte minValue , aber nicht maxValue.
+        //     Wenn minValue gleich maxValue, minValue wird zurückgegeben.
+        //
+        // Ausnahmen:
+        //   T:System.ArgumentOutOfRangeException:
+        //     minValue ist größer als maxValue.
+        public static int Next(int minValueInclusiv, int maxValueExclusive, int not = int.MaxValue)
+        {
+            int ret = 0;
+            do
+            {
+                ret = r.Next(minValueInclusiv, maxValueExclusive);
+            } while (ret == not);
+
+            return ret;
+        }
+        //
+        // Zusammenfassung:
+        //     Gibt eine nicht negative Zufallsganzzahl zurück, die kleiner als das angegebene
+        //     Maximum ist.
+        //
+        // Parameter:
+        //   maxValue:
+        //     Die exklusive obere Grenze der Zufallszahl generiert werden soll. maxValuemuss
+        //     größer als oder gleich 0 sein.
+        //
+        // Rückgabewerte:
+        //     Eine 32-Bit-Ganzzahl mit Vorzeichen, die größer als oder gleich 0 und kleiner
+        //     ist als maxValue; d. h. des Bereichs von Werten in der Regel 0 umfasst jedoch
+        //     nicht maxValue. Jedoch wenn maxValue gleich 0 ist, maxValue zurückgegeben wird.
+        //
+        // Ausnahmen:
+        //   T:System.ArgumentOutOfRangeException:
+        //     maxValue ist kleiner als 0.
+        public static int Next(int maxValue)
+        {
+            return r.Next(maxValue);
+        }
+        //
+        // Zusammenfassung:
+        //     Füllt die Elemente eines angegebenen Bytearrays mit Zufallszahlen.
+        //
+        // Parameter:
+        //   buffer:
+        //     Ein Array von Bytes, die Zufallszahlen enthalten.
+        //
+        // Ausnahmen:
+        //   T:System.ArgumentNullException:
+        //     buffer ist null.
+        public static void NextBytes(byte[] buffer)
+        {
+            r.NextBytes(buffer);
+        }
+        //
+        // Zusammenfassung:
+        //     Gibt eine zufällige Gleitkommazahl zurück, die größer oder gleich 0,0 und kleiner
+        //     als 1,0 ist.
+        //
+        // Rückgabewerte:
+        //     Eine Gleitkommazahl mit doppelter Genauigkeit, die größer oder gleich 0,0 und
+        //     kleiner als 1,0 ist.
+        public static double NextDouble()
+        {
+            return r.NextDouble();
+        }
+
+    }
+
+}
