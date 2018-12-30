55fd827fac9979b870062c4621ea8d52370d96bc
Tobias Pabst <TobivanHelsinki@live.de>
2018-12-30 10:22:02 +0100

    added android file load

--- TAPPLICATION_Droid/IO.cs
+++ TAPPLICATION_Droid/IO.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using TAPPLICATION.IO;
+using TLIB;
 
 namespace TAPPLICATION_Droid
 {
     class IO : StandardIO, IPlatformIO
     {
-        public Task CreateFolder(DirectoryInfo Info)
+        public override async Task<string> LoadFileContent(FileInfo Info)
+        {
+            if (Cache.TryGetValue(Info.FullName, out string retval))
+            {
+                return retval;
+            }
+            else
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+        public async Task CreateFolder(DirectoryInfo Info)
         {
             throw new NotImplementedException();
         }
@@ -18,27 +33,46 @@ namespace TAPPLICATION_Droid
             throw new NotImplementedException();
         }
 
-        public Task<string> GetCompleteInternPath(Place place)
+        public async Task<string> GetCompleteInternPath(Place place)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<FileInfo>> GetFiles(DirectoryInfo Info, IEnumerable<string> FileTypes = null)
+        public override async Task<IEnumerable<ExtendetFileInfo>> GetFiles(DirectoryInfo Info, IEnumerable<string> FileTypes = null)
         {
             throw new NotImplementedException();
         }
 
-        public Task<bool> OpenFolder(DirectoryInfo Info)
+        public async Task<bool> OpenFolder(DirectoryInfo Info)
         {
             throw new NotImplementedException();
         }
-
-        public Task<FileInfo> PickFile(IEnumerable<string> lststrFileEndings, string Token = null)
+        static Dictionary<string, string> Cache = new Dictionary<string, string>();
+        static void AddToCache(string key, string data)
         {
-            throw new NotImplementedException();
+            if (Cache.Count > 2)
+            {
+                Cache.Remove(Cache.Keys.First());
+            }
+            Cache.Add(key, data);
+        }
+        public async Task<FileInfo> PickFile(IEnumerable<string> lststrFileEndings, string Token = null)
+        {
+            FileData fileData = await CrossFilePicker.Current.PickFile();
+            if (fileData != null)
+            {
+                var file = new FileInfo(Path.Combine(fileData.FilePath, fileData.FileName));
+                var contents = System.Text.Encoding.UTF8.GetString(fileData.DataArray);
+                AddToCache(file.FullName, contents);
+                return file;
+            }
+            else
+            {
+                throw new IsOKException();
+            }
         }
 
-        public Task<DirectoryInfo> PickFolder(string Token = null)
+        public async Task<DirectoryInfo> PickFolder(string Token = null)
         {
             throw new NotImplementedException();
         }
