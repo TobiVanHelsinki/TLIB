7c2ca05462add267aea287960e4981d3e449cb50
Tobi van Helsinki <TobivanHelsinki@live.de>
2019-07-08 15:45:37 +0200

    added comments

--- TLIB/Application/FileInfoExtension.cs
+++ TLIB/Application/FileInfoExtension.cs
@@ -1,31 +1,61 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace TLIB
 {
-
+    /// <summary>
+    /// Provides Extensional Methods for the FileInfo class
+    /// </summary>
     public static class FileInfoExtension
     {
-        public static void ChangeName(this FileInfo File, string Name)
+        /// <summary>
+        /// returns a new FileInfo, that has the given name
+        /// </summary>
+        /// <param name="File"></param>
+        /// <param name="Name"></param>
+        public static FileInfo ChangeName(this FileInfo File, string Name)
         {
-            if (System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
-            File = new FileInfo(File.Directory.FullName + File.Name);
+            return new FileInfo(
+                File.Directory.FullName 
+                + (File.Directory.FullName.EndsWith(Environment.NewLine) ? "" : Environment.NewLine)
+                + Name);
         }
 
+        /// <summary>
+        /// Create a copy of this instance 
+        /// </summary>
+        /// <param name="File"></param>
+        /// <returns></returns>
         public static FileInfo Clone(this FileInfo File)
         {
             return new FileInfo(File.FullName);
         }
 
+        /// <summary>
+        /// Create a copy of this instance 
+        /// </summary>
+        /// <param name="Dir"></param>
+        /// <returns></returns>
         public static DirectoryInfo Clone(this DirectoryInfo Dir)
         {
             return new DirectoryInfo(Dir.FullName);
         }
 
+        /// <summary>
+        /// returns the Fulle Path of this File
+        /// </summary>
+        /// <param name="File"></param>
+        /// <returns></returns>
         public static string Path(this FileInfo File)
         {
             return File.Directory.FullName + System.IO.Path.DirectorySeparatorChar;
         }
 
+        /// <summary>
+        /// returns the Fulle Path of this Directory
+        /// </summary>
+        /// <param name="Dir"></param>
+        /// <returns></returns>
         public static string Path(this DirectoryInfo Dir)
         {
             return Dir.FullName + System.IO.Path.DirectorySeparatorChar;
