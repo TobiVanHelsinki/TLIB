af5947d98254538741ef689c04f5610de18f4e2b
Tobi van Helsinki <TobivanHelsinki@live.de>
2019-10-03 22:36:23 +0200

    added tlib choice for xamarin

---
+++ TLIBChoice.Xamarin/TLIBChoice_Xamarin.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TLIB;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TLIB.Choice.Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TLIBChoice_Xamarin : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public TLIBChoice_Xamarin()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Property to determine how many columns the ui have for the choices
+        /// </summary>
+        public static void Log_DisplayQuestionRequested(string title, string text, ResultCallback choice, object[] choices, Options options)
+        {
+            var ctrl = new TLIBChoice_Xamarin();
+
+            ctrl.MainText.Text = text;
+            ctrl.TitleText.Text = title;
+            ctrl.MinimumWidthRequest = text.Length * 3.4;
+            int optionscounter = 0;
+            int buttonColumns = options.ButtonColumns;
+            if (buttonColumns < 1)
+            {
+                buttonColumns = 3;
+            }
+            for (int i = 0; i < buttonColumns; i++)
+            {
+                if (options.ButtonOrientation == Orientation.Horizontal)
+                {
+                    ctrl.OptionsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                }
+                else
+                {
+                    ctrl.OptionsGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                }
+            }
+            for (int i = 0; i < Math.Ceiling(choices.Length / (double)buttonColumns); i++)
+            {
+                if (options.ButtonOrientation == Orientation.Horizontal)
+                {
+                    ctrl.OptionsGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                }
+                else
+                {
+                    ctrl.OptionsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+                }
+            }
+
+            ctrl.OptionsStack.Orientation = options.ButtonOrientation == Orientation.Horizontal ? StackOrientation.Horizontal : StackOrientation.Vertical;
+            foreach (var item in choices)
+            {
+                var b = new Button
+                {
+                    Text = item.ToString(),
+                };
+                b.Resources.Add(nameof(optionscounter), optionscounter);
+                b.Clicked += (s, e) => { choice.SendResultNo((int)(s as Button).Resources[nameof(optionscounter)]); PopupNavigation.Instance.PopAsync(true); };
+                if (options.ButtonPresentation == Presentation.Stackpanel)
+                {
+                    ctrl.OptionsStack.Children.Add(b);
+                }
+                else
+                {
+                    if (options.ButtonOrientation == Orientation.Horizontal)
+                    {
+                        Grid.SetColumn(b, optionscounter % buttonColumns);
+                        Grid.SetRow(b, optionscounter / buttonColumns);
+                    }
+                    else
+                    {
+                        Grid.SetColumn(b, optionscounter / buttonColumns);
+                        Grid.SetRow(b, optionscounter % buttonColumns);
+                    }
+                    ctrl.OptionsGrid.Children.Add(b);
+                }
+                optionscounter++;
+            }
+            try
+            {
+                PopupNavigation.Instance.PushAsync(ctrl, true);
+            }
+            catch (InvalidOperationException)
+            {
+            }
+        }
+    }
+}
---
+++ TLIBChoice.Xamarin_Test/TLIBChoice.Xamarin_Test/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TLIB;
+using Xamarin.Forms;
+
+namespace TLIBChoice.Xamarin_Test
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        #region NotifyPropertyChanged
+        public new event PropertyChangedEventHandler PropertyChanged;
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        string _Title = "ATTANTION! Importanté!";
+        public string Caption
+        {
+            get { return _Title; }
+            set { if (_Title != value) { _Title = value; NotifyPropertyChanged(); } }
+        }
+
+        string _Text = "This is a very important choice you have to made, young padawan. Choose wise and an not unimportant amount of money will be granted to you.";
+        public string Text
+        {
+            get { return _Text; }
+            set { if (_Text != value) { _Text = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(TextLength)); } }
+        }
+        public int TextLength => _Text.Length;
+
+
+        int _Columns;
+        public int Columns
+        {
+            get { return _Columns; }
+            set { if (_Columns != value) { _Columns = value; NotifyPropertyChanged(); } }
+        }
+
+        int _Presentation;
+        public int Presentation
+        {
+            get { return _Presentation; }
+            set { if (_Presentation != value) { _Presentation = value; NotifyPropertyChanged(); } }
+        }
+        int _Orientation;
+        public int Orientation
+        {
+            get { return _Orientation; }
+            set { if (_Orientation != value) { _Orientation = value; NotifyPropertyChanged(); } }
+        }
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Log.DisplayChoiceRequested += TLIB.Choice.Xamarin.TLIBChoice_Xamarin.Log_DisplayQuestionRequested;
+            BindingContext = this;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            Log.DisplayChoice(Caption, Text
+                , new Options() { ButtonColumns = Columns, ButtonOrientation = (Orientation)Orientation, ButtonPresentation = (Presentation)Presentation },
+                ("Opt1", () => Result.Text = "Opt1"),
+                ("Opt2", () => Result.Text = "Opt2"),
+                ("Opt3", () => Result.Text = "Opt3"),
+                ("Opt4", () => Result.Text = "Opt4"),
+                ("Opt5", () => Result.Text = "Opt5"),
+                ("Opt6", () => Result.Text = "Opt6"),
+                ("Opt7", () => Result.Text = "Opt7"),
+                ("Opt8", () => Result.Text = "Opt8")
+                );
+        }
+    }
+}
